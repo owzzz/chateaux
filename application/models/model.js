e40c0a1a70b3b9a5767013166907417b97aaa748
Owain Lleweellyn <owain.llew@gmail.com>
2014-04-18 16:31:22 -0400

    added Schema

--- application/models/model.js
+++ application/models/model.js
@@ -1,3 +1,35 @@
 var Mongoose = require('mongoose');
 var Schema = Mongoose.schema;
 
+var VenueSchema = new Schema({
+  name:  String,
+  description: String,
+  contact: {
+  	phone: String,
+  	email: String
+  },
+  categories: { 
+  	grapeVariety: String, 
+  	classification: String 
+  },
+  location: {
+	address: String,
+	crossStreet : String,
+	lat : Number,
+	lng : Number,
+	postalCode : String,
+	cc : String,
+	city : String,
+	state : String,
+	country : String
+  },
+  url: String,
+  timeFrames: [{
+  	days: String,
+  	open: [{
+  		renderedtime: String
+  	}]
+  }]
+});
+
+module.exports = mongoose.model('Venue', VenueSchema);
